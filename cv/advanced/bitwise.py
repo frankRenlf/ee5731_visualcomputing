817eea49501afadf5cf89949f0d990131c5cc786
frankRenlf <frank.renlingfeng@gmail.com>
2023-09-06 15:38:51 +0800

    update

--- cv/advanced/bitwise.py
+++ cv/advanced/bitwise.py
@@ -13,13 +13,15 @@ import cv2 as cv
 import numpy as np
 
 if __name__ == "__main__":
-    img = cv.imread('../data/img/1_cat.jpg')
-    medianBlur = cv.medianBlur(img, 7)
-    bilateralBlur = cv.bilateralFilter(img, 7, 15, 15)
+    # img = cv.imread('../data/img/1_cat.jpg')
+    blank = np.zeros((400, 400), dtype='uint8')
+    img1 = cv.rectangle(blank.copy(), (30, 30), (370, 370), (255, 255, 255), -1)
+    img2 = cv.circle(blank.copy(), (200, 200), 200, (255, 255, 255), -1)
+    bitwise_and = cv.bitwise_and(img1, img2)
     while True:
-        cv.imshow('bilateralBlur', bilateralBlur)
-        cv.imshow('medianBlur', medianBlur)
-        cv.imshow('origin', img)
+        cv.imshow('rectangle', img1)
+        cv.imshow('circle', img2)
+        cv.imshow('bitwise_and', bitwise_and)
         if cv.waitKey(1) == ord('q'):
             break
     cv.destroyAllWindows()
