4ba4cd06beed26a7844680f3334e81a4b3e8829a
frankRenlf <frank.renlingfeng@gmail.com>
2023-09-06 15:46:43 +0800

    update

--- cv/advanced/bitwise.py
+++ cv/advanced/bitwise.py
@@ -13,19 +13,24 @@ import cv2 as cv
 import numpy as np
 
 if __name__ == "__main__":
-    # img = cv.imread('../data/img/1_cat.jpg')
+    img = cv.imread('../data/img/1_cat.jpg')
     blank = np.zeros((400, 400), dtype='uint8')
     img1 = cv.rectangle(blank.copy(), (30, 30), (370, 370), (255, 255, 255), -1)
     img2 = cv.circle(blank.copy(), (200, 200), 200, (255, 255, 255), -1)
     bitwise_and = cv.bitwise_and(img1, img2)
     bitwise_or = cv.bitwise_or(img1, img2)
     bitwise_xor = cv.bitwise_xor(img1, img2)
+
+    img_crop1 = img[0:200, 0:200]
+    img_crop2 = img[100:300, 100:300]
+    img_xor = cv.bitwise_xor(img_crop1, img_crop2)
     while True:
         cv.imshow('rectangle', img1)
         cv.imshow('circle', img2)
         cv.imshow('bitwise_and', bitwise_and)
         cv.imshow('bitwise_or', bitwise_or)
         cv.imshow('bitwise_xor', bitwise_xor)
+        cv.imshow('img_xor', img_xor)
         if cv.waitKey(1) == ord('q'):
             break
     cv.destroyAllWindows()
