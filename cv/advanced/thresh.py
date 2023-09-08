bc0ba6ff179cbdb6ec6719fc988532ed3e0d1967
frankRenlf <frank.renlingfeng@gmail.com>
2023-09-08 18:54:00 +0800

    update

--- cv/advanced/thresh.py
+++ cv/advanced/thresh.py
@@ -16,14 +16,19 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     img = cv.imread('../data/img/1_cat.jpg')
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
+    # simple threshold
     threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
     threshold_inv, thresh_inv = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV)
+    # adaptive threshold
+    thresh_adaptive = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 3)
+    thresh_adaptive_inv = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 11, 3)
     print(threshold, threshold_inv)
     while True:
+        cv.imshow('gray', gray)
         cv.imshow('thresh', thresh)
         cv.imshow('thresh_inv', thresh_inv)
-        cv.imshow('gray', gray)
+        cv.imshow('thresh_adaptive', thresh_adaptive)
+        cv.imshow('thresh_adaptive_inv', thresh_adaptive_inv)
         if cv.waitKey(1) == ord('q'):
             break
     cv.destroyAllWindows()
